0989f651dbc21bac8d61b5d467b43bacc6ef8992
swarajhoster <yuvrajsimasingh@gmail.com>
2022-09-13 18:59:45 +0530

    wraped onBoard screen with SingleChildScrollView

--- lib/features/onboard/screen/onboarding_screen.dart
+++ lib/features/onboard/screen/onboarding_screen.dart
@@ -10,68 +10,70 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: SafeArea(
-          child: IntroductionScreen(
-            pages: [
-              PageViewModel(
-                title: 'A reader lives a thousand lives',
-                body: 'The man who never reads lives only one.',
-                image: buildImage(
-                  'https://imaginovation.net/static/b35d3ed9e922415df7c292561d572891/3fcdd/10631-Importance-of-Web-App-1.webp',
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: IntroductionScreen(
+              pages: [
+                PageViewModel(
+                  title: 'A reader lives a thousand lives',
+                  body: 'The man who never reads lives only one.',
+                  image: buildImage(
+                    'https://imaginovation.net/static/b35d3ed9e922415df7c292561d572891/3fcdd/10631-Importance-of-Web-App-1.webp',
+                  ),
+                  decoration: getPageDecoration(),
                 ),
-                decoration: getPageDecoration(),
+                PageViewModel(
+                  title: 'Featured Books',
+                  body: 'Available right at your fingerprints',
+                  image: buildImage(
+                    'https://imaginovation.net/static/c1ea6e5715b2a83f3329817f4bd7be1e/3fcdd/10631-Importance-of-Web-App-2.webp',
+                  ),
+                  decoration: getPageDecoration(),
+                ),
+                PageViewModel(
+                  title: 'Simple UI',
+                  body: 'For enhanced reading experience',
+                  image: buildImage(
+                    'https://miro.medium.com/max/512/1*GaBtlHe240ZkwlcBrFczgQ.jpeg',
+                  ),
+                  decoration: getPageDecoration(),
+                ),
+                PageViewModel(
+                  title: 'Today a reader, tomorrow a leader',
+                  body: 'Start your journey',
+                  footer: OnBoardingSCreenButtonWidget(
+                    text: 'Start Reading',
+                    onClicked: () => goToHome(context),
+                  ),
+                  image: buildImage(
+                    'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg',
+                  ),
+                  decoration: getPageDecoration(),
+                ),
+              ],
+              done: const Text(
+                'Read',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
               ),
-              PageViewModel(
-                title: 'Featured Books',
-                body: 'Available right at your fingerprints',
-                image: buildImage(
-                  'https://imaginovation.net/static/c1ea6e5715b2a83f3329817f4bd7be1e/3fcdd/10631-Importance-of-Web-App-2.webp',
+              onDone: () => goToHome(context),
+              showSkipButton: true,
+              skip: const Text(
+                'Skip',
+                style: TextStyle(
+                  color: Colors.black,
                 ),
-                decoration: getPageDecoration(),
               ),
-              PageViewModel(
-                title: 'Simple UI',
-                body: 'For enhanced reading experience',
-                image: buildImage(
-                  'https://miro.medium.com/max/512/1*GaBtlHe240ZkwlcBrFczgQ.jpeg',
-                ),
-                decoration: getPageDecoration(),
-              ),
-              PageViewModel(
-                title: 'Today a reader, tomorrow a leader',
-                body: 'Start your journey',
-                footer: OnBoardingSCreenButtonWidget(
-                  text: 'Start Reading',
-                  onClicked: () => goToHome(context),
-                ),
-                image: buildImage(
-                  'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg',
-                ),
-                decoration: getPageDecoration(),
-              ),
-            ],
-            done: const Text(
-              'Read',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
+              next: const Icon(
+                Icons.arrow_forward,
                 color: Colors.black,
               ),
+              dotsDecorator: getDotDecoration(),
+              onChange: (index) => print('Page $index selected'),
+              animationDuration: 600,
             ),
-            onDone: () => goToHome(context),
-            showSkipButton: true,
-            skip: const Text(
-              'Skip',
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
-            next: const Icon(
-              Icons.arrow_forward,
-              color: Colors.black,
-            ),
-            dotsDecorator: getDotDecoration(),
-            onChange: (index) => print('Page $index selected'),
-            animationDuration: 600,
           ),
         ),
       );
