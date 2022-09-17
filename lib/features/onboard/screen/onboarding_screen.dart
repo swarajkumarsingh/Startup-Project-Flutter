b31a6a5230898576f585aa3050bb5b1c87bd8abc
swarajhoster <yuvrajsimasingh@gmail.com>
2022-09-17 12:59:35 +0530

    performance improvement in onBoarding Screen

--- lib/features/onboard/screen/onboarding_screen.dart
+++ lib/features/onboard/screen/onboarding_screen.dart
@@ -48,262 +48,199 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: currentState == SCREEN_STATE.LOGIN_STATE
-          ? IntroductionScreen(
-              pages: [
-                PageViewModel(
-                  title: 'A reader lives a thousand lives',
-                  body: 'The man who never reads lives only one.',
-                  image: buildImage(
-                      'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
-                  decoration: getPageDecoration(),
-                ),
-                
-                PageViewModel(
-                  title: 'Featured Books',
-                  body: 'Available right at your fingerprints',
-                  image: buildImage(
-                      'https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?w=2000'),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  title: 'Simple UI',
-                  body: 'For enhanced reading experience',
-                  image: buildImage(
-                      'https://media.istockphoto.com/vectors/happy-young-employees-giving-support-and-help-each-other-vector-id1218490893?k=20&m=1218490893&s=612x612&w=0&h=svJxkZEFiciFHufK4LNn13TpNip1cVPW9Ig0Ahuugqs='),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  titleWidget: Text(
-                    "Login to Welcome 😀",
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                  bodyWidget: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 10),
-                        Form(
-                          key: formKeyEmail,
-                          child: CustomTextField(
-                            text: "Enter your email",
-                            controller: emailController,
-                            textInputType: TextInputType.emailAddress,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Form(
-                          key: formKeyPassword,
-                          child: CustomTextField(
-                            text: "Enter your password",
-                            controller: passwordController,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        CustomButton(
-                            text: "Login",
-                            onPressed: () {
-                              nController.login(
-                                  email: emailController.text,
-                                  password: passwordController.text);
-                            }),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Don't have an account "),
-                            InkWell(
-                              onTap: () {
-                                setState(() {
-                                  currentState = SCREEN_STATE.REGISTER_STATE;
-                                });
-                              },
-                              child: Text(
-                                "register",
-                                style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                  image: buildImage(
-                      'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  title: 'Today a reader, tomorrow a leader',
-                  body: 'Start your journey',
-                  footer: OnBoardingSCreenButtonWidget(
-                    text: 'Login',
-                    onClicked: () => controller.login(),
-                  ),
-                  image: buildImage(
-                      'https://miro.medium.com/max/512/1*GaBtlHe240ZkwlcBrFczgQ.jpeg'),
-                  decoration: getPageDecoration(),
-                ),
-              ],
-              done: InkWell(
-                onTap: () => controller.login(),
-                child: const Text(
-                  'Login',
+        child: IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: 'A reader lives a thousand lives',
+          body: 'The man who never reads lives only one.',
+          image: buildImage(
+              'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: 'Featured Books',
+          body: 'Available right at your fingerprints',
+          image: buildImage(
+              'https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?w=2000'),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: 'Simple UI',
+          body: 'For enhanced reading experience',
+          image: buildImage(
+              'https://media.istockphoto.com/vectors/happy-young-employees-giving-support-and-help-each-other-vector-id1218490893?k=20&m=1218490893&s=612x612&w=0&h=svJxkZEFiciFHufK4LNn13TpNip1cVPW9Ig0Ahuugqs='),
+          decoration: getPageDecoration(),
+        ),
+        currentState == SCREEN_STATE.LOGIN_STATE
+            ? PageViewModel(
+                titleWidget: Text(
+                  "Login to Welcome 😀",
                   style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
-              ),
-              onDone: () => controller.login(),
-              showSkipButton: true,
-              skip: const Text(
-                'Skip',
-                style: TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-              next: const Icon(
-                Icons.arrow_forward,
-                color: Colors.black,
-              ),
-              dotsDecorator: getDotDecoration(),
-              onChange: (index) => debugPrint('Page $index selected'),
-              animationDuration: 600,
-            )
-          : IntroductionScreen(
-              pages: [
-                PageViewModel(
-                  title: 'A reader lives a thousand lives',
-                  body: 'The man who never reads lives only one.',
-                  image: buildImage(
-                      'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  title: 'Featured Books',
-                  body: 'Available right at your fingerprints',
-                  image: buildImage(
-                      'https://img.freepik.com/free-vector/business-team-brainstorm-idea-lightbulb-from-jigsaw-working-team-collaboration-enterprise-cooperation-colleagues-mutual-assistance-concept-pinkish-coral-bluevector-isolated-illustration_335657-1651.jpg?w=2000'),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  title: 'Simple UI',
-                  body: 'For enhanced reading experience',
-                  image: buildImage(
-                      'https://media.istockphoto.com/vectors/happy-young-employees-giving-support-and-help-each-other-vector-id1218490893?k=20&m=1218490893&s=612x612&w=0&h=svJxkZEFiciFHufK4LNn13TpNip1cVPW9Ig0Ahuugqs='),
-                  decoration: getPageDecoration(),
-                ),
-                PageViewModel(
-                  titleWidget: Text(
-                    "Login to Welcome 😀",
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
-                  bodyWidget: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        CustomTextField(
-                          text: "Enter your Name",
-                          controller: nameController,
-                        ),
-                        const SizedBox(height: 10),
-                        CustomTextField(
+                bodyWidget: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      Form(
+                        key: formKeyEmail,
+                        child: CustomTextField(
                           text: "Enter your email",
                           controller: emailController,
+                          textInputType: TextInputType.emailAddress,
                         ),
-                        const SizedBox(height: 10),
-                        CustomTextField(
+                      ),
+                      const SizedBox(height: 10),
+                      Form(
+                        key: formKeyPassword,
+                        child: CustomTextField(
                           text: "Enter your password",
                           controller: passwordController,
                         ),
-                        const SizedBox(height: 20),
-                        CustomButton(
-                            text: "Register",
-                            onPressed: () {
-                              nController.register(
-                                name: nameController.text,
+                      ),
+                      const SizedBox(height: 20),
+                      CustomButton(
+                          text: "Login",
+                          onPressed: () {
+                            nController.login(
                                 email: emailController.text,
-                                password: passwordController.text,
-                              );
-                            }),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Already Have an account "),
-                            InkWell(
-                              onTap: () {
-                                setState(() {
-                                  currentState = SCREEN_STATE.LOGIN_STATE;
-                                });
-                              },
-                              child: Text(
-                                "login",
-                                style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
-                                  fontWeight: FontWeight.bold,
-                                ),
+                                password: passwordController.text);
+                          }),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have an account "),
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                currentState = SCREEN_STATE.REGISTER_STATE;
+                              });
+                            },
+                            child: Text(
+                              "register",
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ],
-                        )
-                      ],
-                    ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                  image: buildImage(
-                      'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
-                  decoration: getPageDecoration(),
                 ),
-                PageViewModel(
-                  title: 'Today a reader, tomorrow a leader',
-                  body: 'Start your journey',
-                  footer: OnBoardingSCreenButtonWidget(
-                    text: 'Login',
-                    onClicked: () => controller.login(),
-                  ),
-                  image: buildImage(
-                      'https://miro.medium.com/max/512/1*GaBtlHe240ZkwlcBrFczgQ.jpeg'),
-                  decoration: getPageDecoration(),
-                ),
-              ],
-              done: InkWell(
-                onTap: () => controller.login(),
-                child: const Text(
-                  'Login',
+                image: buildImage(
+                    'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
+                decoration: getPageDecoration(),
+              )
+            : PageViewModel(
+                titleWidget: Text(
+                  "Login to Welcome 😀",
                   style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
-              ),
-              onDone: () => controller.login(),
-              showSkipButton: true,
-              skip: const Text(
-                'Skip',
-                style: TextStyle(
-                  color: Colors.black,
+                bodyWidget: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomTextField(
+                        text: "Enter your Name",
+                        controller: nameController,
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        text: "Enter your email",
+                        controller: emailController,
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextField(
+                        text: "Enter your password",
+                        controller: passwordController,
+                      ),
+                      const SizedBox(height: 20),
+                      CustomButton(
+                          text: "Register",
+                          onPressed: () {
+                            nController.register(
+                              name: nameController.text,
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                          }),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Already Have an account "),
+                          InkWell(
+                            onTap: () {
+                              setState(() {
+                                currentState = SCREEN_STATE.LOGIN_STATE;
+                              });
+                            },
+                            child: Text(
+                              "login",
+                              style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
+                image: buildImage(
+                    'https://www.pyramidions.com/blog/wp-content/uploads/2020/04/technology-stack-for-web-application-main.jpg'),
+                decoration: getPageDecoration(),
               ),
-              next: const Icon(
-                Icons.arrow_forward,
-                color: Colors.black,
-              ),
-              dotsDecorator: getDotDecoration(),
-              onChange: (index) => debugPrint('Page $index selected'),
-              animationDuration: 600,
-            ),
-    );
+        PageViewModel(
+          title: 'Today a reader, tomorrow a leader',
+          body: 'Start your journey',
+          footer: OnBoardingSCreenButtonWidget(
+            text: 'Login',
+            onClicked: () => controller.login(),
+          ),
+          image: buildImage(
+              'https://miro.medium.com/max/512/1*GaBtlHe240ZkwlcBrFczgQ.jpeg'),
+          decoration: getPageDecoration(),
+        ),
+      ],
+      done: InkWell(
+        onTap: () => controller.login(),
+        child: const Text(
+          'Login',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onDone: () => controller.login(),
+      showSkipButton: true,
+      skip: const Text(
+        'Skip',
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+      next: const Icon(
+        Icons.arrow_forward,
+        color: Colors.black,
+      ),
+      dotsDecorator: getDotDecoration(),
+      onChange: (index) => debugPrint('Page $index performance'),
+      animationDuration: 600,
+    ));
   }
 
   Widget buildImage(String uri) {
@@ -316,6 +253,8 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       ),
     );
   }
+
+
 
   DotsDecorator getDotDecoration() {
     return DotsDecorator(
