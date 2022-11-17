c1aba06de86291b7ffca9a90587e0f72d51a8606
swarajhoster <yuvrajsimasingh@gmail.com>
2022-11-17 18:42:42 +0530

    more into app error handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
-import 'dart:async';
 import 'dart:io';
+import 'dart:async';
 import 'dart:isolate';
 
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:restart_app/restart_app.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:starter_project_flutter/config.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'package:starter_project_flutter/my_app.dart';
+import 'package:starter_project_flutter/config.dart';
 import 'package:starter_project_flutter/utils/re_start_app_widget.dart';
 
 main() => init();
@@ -22,13 +23,20 @@ void init() async {
     /// Init Firebase
     await Firebase.initializeApp();
 
-    /// Report Crash to FirebaseCrashlytics
+    /// * Report Crash Multiple Crashes to FirebaseCrashlytics
+    /// * Reference Flutter Error Handling & FirebaseCrashlytics Official Documentation
     if (isDebugMode == false) {
       FlutterError.onError =
           FirebaseCrashlytics.instance.recordFlutterFatalError;
-    }
 
-    /// Send error which are not captured by flutter & report to firebase
+      
+    /// [Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics]
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+
+    /// [Send error which are not captured by flutter & report to firebase]
     Isolate.current.addErrorListener(RawReceivePort((pair) async {
       final List<dynamic> errorAndStacktrace = pair;
       if (isDebugMode == false) {
@@ -41,12 +49,14 @@ void init() async {
       if (isDebugMode) Restart.restartApp();
     }).sendPort);
 
-    /// Re Start App when error occurs.
+    /// [Re Start App when error occurs.]
     ErrorWidget.builder = (FlutterErrorDetails details) {
       FlutterError.presentError(details);
-      if (isDebugMode) Restart.restartApp();
+      Restart.restartApp();
       return Container();
     };
+    }
+
 
     /// Init GetStorage
     await GetStorage.init();
